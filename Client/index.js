08fcf34cfb2c240c74e12c55114c71975dc778ad
orcas <7555ic@gmail.com>
2016-01-23 17:19:13 +0800

    Client stuff

---
+++ Client/index.js
@@ -0,0 +1,25 @@
+var net = require("net");
+var fs = require("fs");
+var path = require("path");
+var cryto = require("crypto");
+
+var filepath = path.join(__dirname, "id.bin");
+
+var socket = net.Socket()
+
+socket.connect((socket) => {
+	var id;
+	fs.readFile(filepath, (err, data) => {
+		if(err){
+			id = crypto.randomBytes(4);
+		}else{
+			id = data;
+		}
+	});
+	socket.write(0x41);
+	socket.write(id);
+	while(socket.read() != 0x51){}
+	setInterval(() -> {
+		socket.write(0x40);
+	}, 500);
+});
