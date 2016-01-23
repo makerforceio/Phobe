287d36ffa9bb44708e3925db682c6c2d166e4f5f
orcas <7555ic@gmail.com>
2016-01-23 18:06:06 +0800

    Serial ports.

--- Client/index.js
+++ Client/index.js
@@ -1,13 +1,19 @@
-var net = require("net");
-var fs = require("fs");
-var path = require("path");
 var cryto = require("crypto");
+var fs = require("fs");
+var net = require("net");
+var path = require("path");
+
+var serialport = require("serialport")
+
+var serialPort = new serialport.SerialPort("/dev/ttyACM0", {
+	baud_rate:115200
+});
 
 var filepath = path.join(__dirname, "id.bin");
 
 var socket = net.Socket()
 
-socket.on("connect", (socket) => {
+socket.on("connect", (socket) -> {
 	var id;
 	fs.readFile(filepath, (err, data) => {
 		if(err){
@@ -19,10 +25,20 @@ socket.on("connect", (socket) => {
 	socket.write(0x41);
 	socket.write(id);
 	socket.on("data", (err, data) => {
-		if(data == 0x51){
-			setInterval(() -> {
-				socket.write(0x40);
-			}, 500);
+		switch(data[0]){
+			case 0x51:
+				setInterval(() => {
+					socket.write(0x40);
+				}, 500);
+			break;
+			case 0x54:
+				serialPort.write(data);
+			break;
+			case 0x58:
+				serialPort.write(data);
+			break;
+			default:
+			break;
 		}
 	});
 });
