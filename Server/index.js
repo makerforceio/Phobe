47afc783d1fac0fa328f81316700ca2b6998321c
Ambrose Chua <ambrose.chua.cm@gmail.com>
2016-01-23 18:13:45 +0800

    add server

---
+++ Server/index.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var expresshbs = require("express-handlebars");
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://phobe:phobe123@localhost/phobe');
